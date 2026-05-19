d7c8ee65ab05f202e6b48e35f35638ec5063324a
Hajisthanakhath <S92086738@ousl.lk>
2026-05-19 20:53:56 +0530

    Force dynamic rendering to fix suspense prerender error

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = 'force-dynamic';
 import React, { useState, useMemo, Suspense } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";

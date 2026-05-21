17921330d357c334ce578585337aba0cc89ca3fd
Mahathir-Mohamed-Eng <mahathirmohamed520@gmail.com>
2026-05-21 18:40:35 +1000

    Added Google OAuth signin integration

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -15,7 +15,7 @@ export default function SignInPage() {
         </p>
 
         <button
-          onClick={() => alert("Google Sign In Working")}
+          onClick={() => signIn("google")}
           className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-full font-medium transition"
         >
           Sign in with Google
